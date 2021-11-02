3d89974c3b1c334c7aaa19c993ef7b8c20b21976
Mehdi Minaee <1332834+mehdiminaee@users.noreply.github.com>
2021-11-02 23:52:01 +0330

    Initialize net5

---
+++ RazorSvelte/App/shared/urls.ts
@@ -0,0 +1,15 @@
+﻿import { getFromJson } from "./config";
+
+const urls = getFromJson<{
+    authorizedUrl: string;
+    indexUrl: string;
+    loginUrl: string;
+    logoutUrl: string;
+    privacyUrl: string;
+    spaUrl: string;
+    signInGoogleUrl: string;
+    signInLinkedInUrl: string;
+    signInGitHubUrl: string;
+}>("urls");
+
+export default urls;
